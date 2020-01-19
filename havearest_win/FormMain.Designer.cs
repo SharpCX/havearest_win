7ae451be9fd4c862224475f536e19c428a8b4e0d
cx <cx@DESKTOP-61CO2A0>
2020-01-19 17:29:17 +0800

    init

---
+++ havearest_win/FormMain.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace havearest_win
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lab_release = new System.Windows.Forms.Label();
+            this.txt_time = new System.Windows.Forms.TextBox();
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.timer_release = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_pause = new System.Windows.Forms.Button();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lab_release
+            // 
+            this.lab_release.AutoSize = true;
+            this.lab_release.Font = new System.Drawing.Font("微软雅黑", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lab_release.ForeColor = System.Drawing.Color.Coral;
+            this.lab_release.Location = new System.Drawing.Point(76, 19);
+            this.lab_release.Name = "lab_release";
+            this.lab_release.Size = new System.Drawing.Size(299, 124);
+            this.lab_release.TabIndex = 0;
+            this.lab_release.Text = "00:00";
+            // 
+            // txt_time
+            // 
+            this.txt_time.Font = new System.Drawing.Font("宋体", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txt_time.Location = new System.Drawing.Point(25, 190);
+            this.txt_time.Name = "txt_time";
+            this.txt_time.Size = new System.Drawing.Size(195, 44);
+            this.txt_time.TabIndex = 1;
+            this.txt_time.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_time_KeyPress);
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.Font = new System.Drawing.Font("宋体", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_ok.Location = new System.Drawing.Point(301, 190);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(119, 44);
+            this.btn_ok.TabIndex = 2;
+            this.btn_ok.Text = "确定";
+            this.btn_ok.UseVisualStyleBackColor = true;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
+            // 
+            // timer_release
+            // 
+            this.timer_release.Interval = 1000;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(226, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 36);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Min";
+            // 
+            // btn_pause
+            // 
+            this.btn_pause.Font = new System.Drawing.Font("宋体", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_pause.Location = new System.Drawing.Point(25, 260);
+            this.btn_pause.Name = "btn_pause";
+            this.btn_pause.Size = new System.Drawing.Size(195, 44);
+            this.btn_pause.TabIndex = 4;
+            this.btn_pause.Text = "暂停";
+            this.btn_pause.UseVisualStyleBackColor = true;
+            this.btn_pause.Click += new System.EventHandler(this.btn_pause_Click);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Font = new System.Drawing.Font("宋体", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_start.Location = new System.Drawing.Point(233, 260);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(187, 44);
+            this.btn_start.TabIndex = 5;
+            this.btn_start.Text = "开始";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 351);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.btn_pause);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_ok);
+            this.Controls.Add(this.txt_time);
+            this.Controls.Add(this.lab_release);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "have a rest for lynn";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lab_release;
+        private System.Windows.Forms.TextBox txt_time;
+        private System.Windows.Forms.Button btn_ok;
+        private System.Windows.Forms.Timer timer_release;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_pause;
+        private System.Windows.Forms.Button btn_start;
+    }
+}
+
---
+++ havearest_win/FormMain.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace havearest_win
+{
+    public partial class FormMain : Form
+    {
+
+        [DllImport("user32 ")]
+        public static extern bool LockWorkStation();
+        
+        private int time_long_init = 30;
+        private int time_long = 30;
+        private string time_show = "30:00";
+
+        public FormMain()
+        {
+            InitializeComponent();
+            this.reinit(this.loadTimeFromFile());
+            timer_release.Start();
+            timer_release.Tick += Timer_release_Tick;
+        }
+
+        private void Timer_release_Tick(object sender, EventArgs e)
+        {
+            if (time_long > 0)
+            {
+                this.time_long--;
+                refreshLabel();
+            } else {
+                LockWorkStation();
+                timer_release.Stop();
+                this.reinit(this.time_long_init);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void restart() {
+            this.timer_release.Start();
+        }
+
+        private void reinit(int time_long) {
+            this.time_long_init = time_long;
+            this.time_long = time_long;
+            this.refreshLabel();
+        }
+
+        private void refreshLabel()
+        {
+            int minit_num = this.time_long / 60;
+            int second_num = this.time_long % 60;
+            this.lab_release.Text = minit_num.ToString("D2") + ":" + second_num.ToString("D2");
+
+        }
+
+        private void txt_time_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.')) {
+                e.Handled = true;
+            }
+            if ((e.KeyChar != '.') && ((sender as TextBox).Text.IndexOf('.')>-1))
+            {
+                e.Handled = true;
+            }
+        }
+        
+        private void btn_ok_Click(object sender, EventArgs e)
+        {
+            int tmp_long_init = int.Parse(this.txt_time.Text);
+            if (tmp_long_init < 0 || tmp_long_init>100)
+            {
+                MessageBox.Show("数字范围1-99");
+                return;
+            }
+            this.reinit(tmp_long_init*60);
+            this.writeFile(tmp_long_init*60);
+        }
+
+        private void btn_pause_Click(object sender, EventArgs e)
+        {
+            this.timer_release.Stop();
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            this.timer_release.Start();
+        }
+
+        private void writeFile(int time_long) {
+            string text = time_long.ToString();
+            string base_path = Environment.CurrentDirectory;
+            System.IO.File.WriteAllText(this.getConfigFilePath(), text);
+        }
+
+        private int loadTimeFromFile()
+        {
+            String txt = System.IO.File.ReadAllText(this.getConfigFilePath());
+            try
+            {
+                return int.Parse(txt);
+            }
+            catch {
+                return 30 * 60;
+            }
+        }
+
+        private string getConfigFilePath() {
+            string base_path = Environment.CurrentDirectory;
+            string path = System.IO.Path.Combine(base_path, "time_config.conf");
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            return path;
+        }
+    }
+}
